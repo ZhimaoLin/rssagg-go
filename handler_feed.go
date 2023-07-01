866dc5c12bbb266b821deeec95b4d6a15abf6e22
Zhimao Lin <zhimaolin@outlook.com>
2023-06-30 23:46:35 -0400

    Added get all the feeds

--- handler_feed.go
+++ handler_feed.go
@@ -40,6 +40,11 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 	respondWithJSON(w, 201, databaseFeedToFeed(feed))
 }
 
-// func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-// 	respondWithJSON(w, 200, databaseUserToUser(user))
-// }
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, 404, fmt.Sprintf("Could not get feeds"))
+		return
+	}
+	respondWithJSON(w, 200, databaseFeedsToFeeds(feeds))
+}
